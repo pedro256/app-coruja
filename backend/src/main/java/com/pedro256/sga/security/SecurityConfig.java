68f3862a192411aa0052d9606c34673b7cae2a62
Pedro256 <pedroh93601@gmail.com>
2024-04-03 00:13:16 -0400

    fix: cors configuration backend

--- backend/src/main/java/com/pedro256/sga/security/SecurityConfig.java
+++ backend/src/main/java/com/pedro256/sga/security/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +19,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .csrf( csrf -> csrf.disable())
+                .cors(cors -> cors // Configuração CORS usando corsConfigurer()
+                        .configurationSource(corsConfigurationSource()))
                 .oauth2ResourceServer(oauth -> oauth
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())));
 
         return  httpSecurity.build();
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Permitir qualquer origem
+        configuration.addAllowedMethod("*"); // Permitir todos os métodos HTTP
+        configuration.addAllowedHeader("*"); // Permitir todos os cabeçalhos
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
